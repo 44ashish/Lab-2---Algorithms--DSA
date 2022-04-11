91627eeff3029061b1f6947c76b5569e34e00067
44ashish <99636520+44ashish@users.noreply.github.com>
2022-04-11 20:34:59 +0530

    Add files via upload

---
+++ src/paymoney/transaction/Mainclass.java
@@ -0,0 +1,11 @@
+package paymoney.transaction;
+
+public class Mainclass {
+	
+public static void main(String[] args) {
+		
+		PayMoneyTransactionUI ui = new PayMoneyTransactionUI();
+		ui.init();		
+	}
+
+}
---
+++ src/traveller/currencyspend/Mainclass.java
@@ -0,0 +1,11 @@
+package traveller.currencyspend;
+
+public class Mainclass {
+
+public static void main(String[] args) {
+		
+		TravellerCurrencySpendUI ui = 
+			new TravellerCurrencySpendUI();
+		ui.init();
+	}
+}
